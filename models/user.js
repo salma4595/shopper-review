eff00a00829420d2912918ff020f734b3db868ab
alij <alimjahrami@gmail.com>
2023-12-12 21:30:31 +0300

    update details page, fix authentication errors with user and admin

--- models/user.js
+++ models/user.js
@@ -1,20 +1,20 @@
 const mongoose = require('mongoose')
 const Schema = mongoose.Schema
 
-const userSchema = new Schema({
-name: String,
-googleId:{
-    type:String,
-    required: true
-},
-email: String,
-userType: {
-    type: String,
-    enum: ["admin", "user"],
-    default: "user"
+  const userSchema = new Schema({
+  name: String,
+  googleId:{
+      type:String,
+      required: true
   },
-},{timestamps: true})
+  email: String,
+  userType: {
+      type: String,
+      enum: ["admin", "user"],
+      default: "user"
+    },
+  },{timestamps: true})
 
-const User = mongoose.model('user', userSchema)
+const User = mongoose.model('User', userSchema)
 
 module.exports = User;
--- routes/mall.js
+++ routes/mall.js
@@ -4,19 +4,20 @@ const router= express.Router();
 router.use(express.urlencoded({extended: true}));
 const MallCntrl= require("../controllers/mall");
 const upload = require('../config/multerUploader');
+const ensureLoggedIn = require('../config/ensureLoggedIn');
+const ensureLoggedInAdmin = require('../config/ensureLoggedInAdmin');
 
 // router.use(methodOverride('_method'));
 // Require the auth middleware
-const ensureLoggedIn = require('../config/ensureLoggedIn');
 
 //Routes
-router.get("/add",ensureLoggedIn, MallCntrl.mall_create_get);
-router.post("/add",ensureLoggedIn, upload.fields([{name: 'thumbnail'}, {name: 'mallImages'}]) ,MallCntrl.mall_create_post);
+router.get("/add", ensureLoggedInAdmin, MallCntrl.mall_create_get);
+router.post("/add", ensureLoggedInAdmin, upload.fields([{name: 'thumbnail'}, {name: 'mallImages'}]) ,MallCntrl.mall_create_post);
 router.get("/index", MallCntrl.mall_index_get);
 router.get("/detail", MallCntrl.mall_show_get);
-router.get("/delete",ensureLoggedIn, MallCntrl.mall_delete_get);
-router.get("/edit",ensureLoggedIn, MallCntrl.mall_edit_get);
-router.post("/update",ensureLoggedIn, MallCntrl.mall_update_post);
+router.get("/delete", ensureLoggedInAdmin, MallCntrl.mall_delete_get);
+router.get("/edit", ensureLoggedInAdmin, MallCntrl.mall_edit_get);
+router.post("/update", ensureLoggedInAdmin, MallCntrl.mall_update_post);
 
 
 
