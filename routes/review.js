eeac0d8dd07173afe0725f487212d5b0d3bc23b0
salma45 <salmaa-125@outlook.com>
2023-12-13 09:38:57 +0300

    ensuredlogin admin for review

--- routes/review.js
+++ routes/review.js
@@ -9,8 +9,8 @@ const ensureLoggedInAdmin = require('../config/ensureLoggedInAdmin');
 //Routes
 router.get("/add", ensureLoggedIn, ReviewCntrl.review_create_get);
 router.post("/add", ensureLoggedIn, ReviewCntrl.review_create_post);
-router.get("/index", ReviewCntrl.review_index_get);
-router.get("/detail", ReviewCntrl.review_show_get);
+router.get("/index",ensureLoggedInAdmin, ReviewCntrl.review_index_get);
+router.get("/detail",ensureLoggedInAdmin, ReviewCntrl.review_show_get);
 router.get("/delete", ensureLoggedIn, ReviewCntrl.review_delete_get);
 router.get("/edit", ensureLoggedIn, ReviewCntrl.review_edit_get);
 router.post("/update", ensureLoggedIn, ReviewCntrl.review_update_post);
